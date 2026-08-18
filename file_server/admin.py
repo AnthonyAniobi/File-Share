7b6a1aa47f8f3138e4c04f88ad40d5e51cbfa3cc
AnthonyAniobi <anthonyaniobi198@gmail.com>
2026-08-18 11:18:12 +0100

    refactor: migrate project backend from Django to Flask and implement database migrations with Alembic

--- file_server/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from file_server.models import SharedItem
-
-admin.site.site_header = "File Share Admin"
-admin.site.site_title = "File Share Admin Portal"
-admin.site.index_title = "Welcome to File Share Admin Portal"
-
-admin.site.register(SharedItem)
--- file_server/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from file_server import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('share/', views.share, name='share'),
-    path('delete/<int:pk>/', views.delete_file, name='delete_file'),
-]
